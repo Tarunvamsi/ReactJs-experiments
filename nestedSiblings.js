9173b405a8a5f06fcebadef577a2405e47ab4146
PUSARLA TARUN VAMSI <pusarlatarunvamsi@gmail.com>
2024-05-27 14:27:22 +0530

    add complex nested siblings

--- nestedSiblings.js
+++ nestedSiblings.js
@@ -22,3 +22,35 @@ const nestedSiblings = ReactDOM.createRoot(
   document.getElementById("nestedSiblings")
 );
 nestedSiblings.render(parent1);
+
+
+// nested siblings more complex
+
+/*<div id="parent">
+    <div id="child">
+        <h1>i am from nested sibling h1 child</h1>
+        <h2>i am from nested sibling h2 child</h2>
+    </div>
+
+    <div id="child2">
+        <h1>i am from nested sibling h1 child2</h1>
+        <h2>i am from nested sibling h2 child2</h2>
+    </div>
+</div> */
+
+const parent2 = React.createElement(
+    "div",
+    { id: "parent" },[React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {id:"child1"}, "i am sibling and h1 child1"),
+        React.createElement("h2", {id:"child1"}, "i am sibling and h2 child1"),
+      ]),React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {id:"child2"}, "i am sibling and h1 child2"),
+        React.createElement("h2", {id:"child2"}, "i am sibling and h2 child2"),
+      ])]
+    
+  );
+
+  nestedSiblings.render(parent2);
+// above code looks soo complex and tidy
+// JSX -- makes easy when we want to create tags  , no need of React.createElement()
+
